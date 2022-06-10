e061811f65351ec331629fd364f485abf0a75f3e
Chamal-Peiris <chamal.peiris3g@gmail.com>
2022-06-10 17:25:31 +0530

    Implement DAOFactory

--- src/main/java/lk/ijse/dep8/polling/service/custom/impl/PollServiceImpl.java
+++ src/main/java/lk/ijse/dep8/polling/service/custom/impl/PollServiceImpl.java
@@ -2,13 +2,18 @@ package lk.ijse.dep8.polling.service.custom.impl;
 
 import lk.ijse.dep8.polling.dto.PollDTO;
 import lk.ijse.dep8.polling.service.custom.PollService;
+import lk.ijse.dep8.polling.service.util.JPAUtil;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
 public class PollServiceImpl implements PollService {
     @Override
     public List<PollDTO> listAllPoll() {
-        return null;
+        EntityManager em = JPAUtil.getEntityManageFactory().createEntityManager();
+
+        em.close();
+
     }
 
     @Override
